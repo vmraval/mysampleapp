954d3538b27252d5d324bc1bc3d2353447cffa6e
Viral Raval <vmraval2002@rediffmail.com>
2019-11-17 16:06:41 +0100

    CouseAPI

---
+++ course-api-data/src/main/java/bootdata/topic/Topic.java
@@ -0,0 +1,56 @@
+package bootdata.topic;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Entity
+public class Topic {
+
+	@Id
+	String id;
+	String name;
+	String description;
+	
+	public Topic(){
+		
+	}
+
+	public Topic(String id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+	
+}
